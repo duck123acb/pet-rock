66ef296b2a70b9961f78e7e733e936e83f9fcc71
hephaestushex <81438978+hephaestushex@users.noreply.github.com>
2025-03-15 15:49:59 -0600

    added proper client file

--- client.py
+++ client.py
@@ -1,6 +1,20 @@
 import socket
 
-ip = "192.168.8.255"
+ip = "192.168.8.25"
+port = 4200
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client_socket.connect(ip, 4200)
+client_socket.connect((ip, port))
+
+while True:
+    msg = input("Input message: ")
+    client_socket.sendall(msg.encode())
+    response = client_socket.recv(1024)
+    print(response)
+
+    if msg == "end":
+        break
+
+
+client_socket.close()
