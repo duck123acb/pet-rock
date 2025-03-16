296dd1edaca2ef80c8b089c020e96baa2d7e5a2d
Danush Ramanan <dan.ramanan@gmail.com>
2025-03-16 10:22:00 -0600

    main

--- main.py
+++ main.py
@@ -9,26 +9,31 @@ acts as a client and connects to the pi zero w
 sends the ai prompts to the pi
 '''
 
-ai.make_request(secret.setup_prompt)
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((secret.ip, secret.port))
-
 'testing for the sockets'
-# while True:
-#   msg = input("Input message: ")
-#   client_socket.sendall(msg.encode())
-#   response = client_socket.recv(secret.msg_max_size)
-#   print(response)
+  # while True:
+  #   msg = input("Input message: ")
+  #   client_socket.sendall(msg.encode())
+  #   response = client_socket.recv(secret.msg_max_size)
+  #   print(response)
 
-#   if msg == "end":
-#     break
+  #   if msg == "end":
+  #     break
 
-while True:
-  msg = ai.make_request("give me a line")
-  client_socket.sendall(msg.encode())
+def main():
+  print(ai.make_request(secret.setup_prompt))
 
-  if msg == "end":
-    break
+  client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client_socket.connect((secret.ip, secret.port))
 
-client_socket.close()
+  while True:
+    msg = ai.make_request("give me a line")
+    client_socket.sendall(msg.encode())
+
+    if msg == "end":
+      break
+
+  client_socket.close()
+
+
+if __name__ == "__main__":
+  main()
